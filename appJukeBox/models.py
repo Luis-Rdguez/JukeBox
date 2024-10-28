eac71137530be23e6bfa8829644a0e8114352ead
AsierLoinaz <asier.loinaz@opendeusto.es>
2024-10-28 12:50:07 +0100

    template y cosas

--- appJukeBox/models.py
+++ appJukeBox/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Banda(models.Model):
+    # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField()
+    fechaIni = models.DateField()
+    fechaFin = models.DateTimeField()
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    estilos = models.ManyToManyField(Estilo)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Pais(models.Model):
+    # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+    cod_pais = models.CharField(max_length=2)
+    pais = models.CharField(max_length=30)
+    def __str__(self):
+        return self.pais
+    
+
+class Estilo(models.Model):
+
+    cod_estilo = models.CharField(3)
+    estilo = models.CharField()
+
+    def __str__(self):
+        return self.nombre	
--- appJukeBox/views.py
+++ appJukeBox/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import *
 # Create your views here.
+
+from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import render
+from .models import Departamento, Habilidad, Empleado
+
+#devuelve el listado de empresas
+def index_pais(request):
+	pais = get_list_or_404(Pais.objects.order_by('nombre'))
+	context = {'lista_paiss': pais }
+	return render(request, 'index.html', context) # TODO cambiar el html
+
+#devuelve los datos de un departamento
+def show_pais(request, pais_id):
+	pais = get_object_or_404(Pais, pk=pais_id)
+	context = {'pais': pais } 
+	return render(request, 'detail.html', context)
+
+#devuelve los empelados de un departamento
+def index_bandas(request, pais_id):
+	pais = get_object_or_404(Pais, pk=pais_id)
+	bandas =  pais.banda_set.all()
+	context = {'Pais': pais, 'Bandas' : bandas }
+	return render(request, 'empleados.html', context) # TODO cambiar el html
+
+#devuelve los detalles de un empleado
+def show_banda(request, banda_id):
+	banda = get_object_or_404(Banda, pk=banda_id)
+	estilos =  banda.estilos.all()
+	context = { 'banda': banda, 'estilos' : estilos }
+	return render(request, 'empleado.html', context) # TODO cambiar el html
+
+# Devuelve los detalles de una habilidad
+def show_estilo(request, estilo_id):
+    estilo = get_object_or_404(Estilo, pk=estilo_id)
+    bandas =  estilo.banda_set.all()
+    context = { 'Bandas': bandas, 'estilo' : estilo }
+    return render(request, 'habilidad.html', context) # TODO cambiar el html
