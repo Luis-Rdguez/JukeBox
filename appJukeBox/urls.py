3e36a901eb0569169b49162273b1e889812e8f3c
ikergar7 <iker.g.barranco@opendeusto.es>
2024-11-12 13:01:04 +0100

    urls solucionadas

--- appJukeBox/urls.py
+++ appJukeBox/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('bandas/', views.show_bandas, name='todas_bandas'),   # Esto funciona en track y te muestra todas las bandas
     path('paises/', views.show_paises, name='todos_paises'),   # Esto funciona en track y te muestra todos los paises
     path('paises/<int:pais_id>/', views.show_pais, name='pais'),
-    path('paises/<int:pais_id>/bandas', views.index_bandas, name='bandas'),
     path('bandas/<int:banda_id>/', views.show_banda, name='banda'),
     path('estilos/<int:estilo_id>/', views.show_estilo, name='estilo'),
     path('estilos/', views.show_estilos, name='todos_estilos'), # Esto funciona en estiloso y te muestra todos los estilos
