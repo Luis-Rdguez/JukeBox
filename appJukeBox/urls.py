05b3fd84fb46fd06cbd7534bb966c917a77865ac
AsierLoinaz <asier.loinaz@opendeusto.es>
2024-11-12 10:18:17 +0100

    link al pais y a los estilos desde el about de la banda

--- appJukeBox/urls.py
+++ appJukeBox/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     #path('admin/', admin.site.urls), # TODO
     path('', views.index, name='index'), # Esto funciona en index y te muestra una banda por pais
     path('bandas/', views.show_bandas, name='todas_bandas'),   # Esto funciona en track y te muestra todas las bandas
-    path('paises/<int:pais_id>/', views.show_pais, name='detail'),
+    path('paises/<int:pais_id>/', views.show_pais, name='pais'),
     path('paises/<int:pais_id>/bandas', views.index_bandas, name='bandas'),
     path('bandas/<int:banda_id>/', views.show_banda, name='banda'),
     path('estilos/<int:estilo_id>', views.show_estilo, name='estilo'),
