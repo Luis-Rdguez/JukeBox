fe1b32d13821e4691dfd3bc9fbdd3e2d9990925a
ikergar7 <iker.g.barranco@opendeusto.es>
2024-11-13 13:07:30 +0100

    fotos bien puestas e intento de ajustarlas

--- appJukeBox/models.py
+++ appJukeBox/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Pais(models.Model):
     cod_pais = models.CharField(max_length=2)
     pais = models.CharField(max_length=30)
+    bandera = models.URLField(max_length=700)
 
     def numero_bandas(self):
         return self.banda_set.count()
@@ -25,7 +26,8 @@ class Banda(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=200)
     fechaIni = models.DateField()
-    fechaFin = models.DateField(null=True, blank=True)  
+    fechaFin = models.DateField(null=True, blank=True)
+    foto = models.URLField(max_length=700)
     pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
     estilos = models.ManyToManyField(Estilo)
     
